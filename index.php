b53050128c5031c143b8207155ba2d7ebc8cade3
Ditte01 <ditte.kjaer@hotmail.com>
2026-04-30 23:14:08 +0200

    links

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $cases = [
 
 <nav>
     <div>
-        <img src="ikoner/ForsideMEDIUM.png" alt="hjem"><br>hjem
+        <img src="ikoner/ForsideMEDIUM.png" alt="hjem"><br>Hjem
     </div>
     <div>
         <img src="ikoner/CaseMEDIUM.png" alt="cases"><br>Cases
@@ -34,13 +34,12 @@ $cases = [
 </nav>
 
 <section class="hero">
-    <h2>Now playing: Ditte Frydenreim Kjær</h2>
+    <h1>Now playing: Ditte Frydenreim Kjær</h1>
 
     <div class="hero-content">
         <div class="hero-text">
             <p>
-                Her er din tekst. Erstat med rigtig beskrivelse.
-                Kort intro om hvem du er og hvad du laver.
+                Hey, mit navn er Ditte og til dagligt studerer jeg multimediedesign på Zealand Erhversakademi i Køge.
             </p>
 
             <div class="buttons">
@@ -50,51 +49,52 @@ $cases = [
         </div>
 
         <img src="images/Portræt.png" alt="mig">
+
     </div>
 </section>
 
 <section class="section">
-    <h3>Udvalgte cases</h3>
+    <h2>Udvalgte cases</h2>
 
     <div class="case-grid">
         <?php for($i=0; $i<2; $i++): ?>
-            <div class="case">
+            <a href="case.php?id=<?php echo $i; ?>" class="case">
                 <img src="images/VinylpladeMEDIUM.jpg" alt="case">
                 <div class="case-content">
-                    <div><?php echo $cases[$i]['title']; ?></div>
+                    <div class="case-title"><?php echo $cases[$i]['title']; ?></div>
                     <div class="play">
-                        Afspil case <img class="knap" src="ikoner/PlaybuttonMEDIUM.png">
+                        Afspil case <img class="knap" src="ikoner/PlaybuttonSMALL.png">
                     </div>
 
                 </div>
-            </div>
+            </a>
         <?php endfor; ?>
     </div>
 </section>
 
 <section class="section">
-    <h3>Nyeste cases</h3>
+    <h2>Nyeste cases</h2>
 
     <div class="case-grid">
         <?php for($i=2; $i<4; $i++): ?>
-            <div class="case">
+            <a href="case.php?id=<?php echo $i; ?>" class="case">
                 <img src="images/VinylpladeMEDIUM.jpg" alt="case">
                 <div class="case-content">
-                    <div><?php echo $cases[$i]['title']; ?></div>
+                    <div class="case-title"><?php echo $cases[$i]['title']; ?></div>
                     <div class="play">
-                        Afspil case<div> <img class="knap" src="ikoner/PlaybuttonMEDIUM.png"></div>
-
+                        Afspil case <img src="ikoner/PlaybuttonSMALL.png">
                     </div>
                 </div>
-            </div>
+            </a>
         <?php endfor; ?>
     </div>
 </section>
 
+
 <footer>
-    <a href="#">LinkedIn</a>
-    <a href="#">Instagram</a>
-    <a href="#">Facebook</a>
+    <a href="https://www.linkedin.com/in/ditte-frydenreim-kj%C3%A6r-99269a2ba/" target="_blank">LinkedIn</a>
+    <a href="https://www.instagram.com/ditte.f.kjaer/" target="_blank">Instagram</a>
+    <a href="https://www.facebook.com/profile.php?id=100008896668161" target="_blank">Facebook</a>
     <a href="#">Til toppen</a> <img src="ikoner/Pil%20m.%20stregMEDIUM.png" alt="top pil">
 </footer>
 
